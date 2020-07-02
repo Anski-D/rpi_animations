b6acf95cc939a900799714d82be98e44b1d6bcc2
Anski-D <david.anscombe@gmail.com>
2020-07-02 17:23:26 +0100

    Rename message.json to settings.json

--- rpi_animations/__main__.py
+++ rpi_animations/__main__.py
@@ -2,7 +2,7 @@ from rpi_animations.text_animator import TextAnimator
 
 if __name__ == '__main__':
     # Message to display
-    message_file = 'message.json'
+    message_file = 'settings.json'
 
     # Create the text animator
     text_animator = TextAnimator(message_file)
