5070577555cd2f03879a0b6731b8a200e552d804
Anski-D <david.anscombe@gmail.com>
2020-07-09 17:08:06 +0100

    Update loop code slightly

--- rpi_animations/text_animator.py
+++ rpi_animations/text_animator.py
@@ -102,7 +102,7 @@ class TextAnimator:
     def _reqd_num_of_messages(self):
         # Work out how many rectangles fit on the screen, pad by 2 because that is the minimum required if the
         # message is wider than the screen.
-        return int(self.screen.get_rect().width / self._messages.sprites()[0].rect.width) + 2
+        return int(self.screen.get_rect().width / min(message.rect.width for message in self._messages.sprites())) + 2
 
     def _update_items(self):
         # Update images
