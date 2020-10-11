43be332d1d9d1e34e84ae3bf61ebb6414586a182
Anski-D <david.anscombe@gmail.com>
2020-10-11 19:46:09 +0100

    Add new test function to test_settings.py

--- tests/rpi_animations/test_settings.py
+++ tests/rpi_animations/test_settings.py
@@ -14,6 +14,40 @@ class TestSettings:
                and settings_with_dummy_input.bg_colour is None \
                and settings_with_dummy_input.text_colour is None
 
+    def test_load_settings(self, monkeypatch):
+        settings_dict = {
+            "colours": "0,0,0;255,255,255",
+            "text": "Test1;Test2;Test3",
+            "typeface": "Serif Regular",
+            "text_size": 7,
+            "text_speed": 7,
+            "outline_width": 7,
+            "outline_colour": "0,0,0",
+            "image_src": "test1.bmp,test2.bmp,test3.bmp",
+            "num_images": 7,
+            "image_change_time": 7,
+            "colour_change_time": 7,
+            "fps": 7
+        }
+        monkeypatch.setattr(Settings, '_load_json', lambda x: settings_dict)
+        monkeypatch.setattr(Settings, '_load_images', lambda x, y: None)
+        settings_dummy = Settings('test.json')
+
+        assert settings_dummy._colours == [(0, 0, 0), (255, 255, 255)] \
+               and settings_dummy._messages == ['Test1', 'Test2', 'Test3'] \
+               and settings_dummy.typeface is None \
+               and settings_dummy.text_size == 7 \
+               and settings_dummy.text_speed == 7 \
+               and settings_dummy.outline_width == 7 \
+               and settings_dummy.outline_colour == (0, 0, 0) \
+               and settings_dummy.num_images == 7 \
+               and settings_dummy.image_change_time == 7.0 \
+               and settings_dummy.colour_change_time == 7.0 \
+               and settings_dummy.fps == 7
+
+        # self._colours = self._split_colours(settings['colours'])
+        # self._load_images(settings['image_src'])
+
     def test_load_json_type(self):
         assert type(Settings('settings.json')._load_json()) == dict
 
