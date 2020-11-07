18ddb8ff6778c7ef62f5faaf4d742cd839615794
Anski-D <david.anscombe@gmail.com>
2020-11-07 17:21:56 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,12 @@ import setuptools
 setuptools.setup(
     name='rpi_animations',
     version='0.0.1',
-    packages=setuptools.find_packages(),
+    packages=setuptools.find_packages(include=['rpi_animations']),
     url='https://github.com/Anski-D/rpi_animations',
     license='MIT',
     author='David Anscombe',
     author_email='dave.anski@gmail.com',
-    description='Text animations for custom messages.'
+    description='Text animations for custom messages.',
+    python_requires='>=3.7',
+    install_requires=['pygame']
 )
