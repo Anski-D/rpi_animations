d27ddf143f880d8ecefd71f5a7517067410191e7
Anski-D <david.anscombe@gmail.com>
2021-01-23 16:05:49 +0000

    Update path to inputs

--- rpi_animations/__main__.py
+++ rpi_animations/__main__.py
@@ -1,12 +1,13 @@
 from rpi_animations.text_animator import TextAnimator
 import sys
+import os
 
 if __name__ == '__main__':
     # Set the settings files name. In future versions, might be able to customise this.
     settings_file = 'settings.json'
 
     # Get the execution path, required for finding resources
-    resources = sys.path[0]
+    resources = os.path.join(sys.path[0], 'inputs')
 
     # Create the text animator
     text_animator = TextAnimator(resources, settings_file)
