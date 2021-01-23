a15071768d557ca0778a6987414387288b4fd639
Anski-D <david.anscombe@gmail.com>
2021-01-23 16:01:31 +0000

    Draft setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='rpi_animations',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests', 'tests.*']),
+    url='https://github.com/Anski-D/rpi_animations',
+    license='MIT',
+    author='David Anscombe',
+    author_email='dave.anski@gmail.com',
+    description='Display animated scrolling text and pictures that periodically move.',
+    long_description=long_description,
+    python_requires='>=3.7',
+    install_requires=['pygame']
+)
