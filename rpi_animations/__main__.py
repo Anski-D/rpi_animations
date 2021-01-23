7de34531bd00d18ecc4a63fb26e9ddad45c8de80
Anski-D <david.anscombe@gmail.com>
2021-01-23 15:53:04 +0000

    Add addition path parameter to __main__.py

--- rpi_animations/__main__.py
+++ rpi_animations/__main__.py
@@ -1,11 +1,15 @@
 from rpi_animations.text_animator import TextAnimator
+import sys
 
 if __name__ == '__main__':
-    # Message to display
+    # Set the settings files name. In future versions, might be able to customise this.
     settings_file = 'settings.json'
 
+    # Get the execution path, required for finding resources
+    resources = sys.path[0]
+
     # Create the text animator
-    text_animator = TextAnimator(settings_file)
+    text_animator = TextAnimator(resources, settings_file)
 
     # Set the animator running
     text_animator.run()
