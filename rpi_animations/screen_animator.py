3ab08c8b4ce34f2974b1e669605e7d6a9a6c1f1f
Anski-D <dave.anski@gmail.com>
2021-02-05 17:22:44 +0000

    Update docstrings

--- rpi_animations/screen_animator.py
+++ rpi_animations/screen_animator.py
@@ -6,8 +6,25 @@ from .settings import Settings
 
 
 class ScreenAnimator:
+    """
+    ScreenAnimator object that sets up and runs the main animation loop of the rpi_animation program.
+
+    Attributes:
+        settings (Settings): A Settings object that loads, stores, and updates settings as the program runs.
+        screen (screen): A pygame screen object that holds the pygame animation features.
+    """
+
     def __init__(self, settings_file: str, debug_mode=False, fps_on=False) -> None:
-        """Initialise the animation, and create resources."""
+        """
+        Initialise ScreenAnimator class with the resource location and settings file.
+
+        Args:
+            settings_file (str): The settings JSON file to where the settings are loaded from.
+            debug_mode (bool): Boolean switch for activating debug mode which will run the program in windowed mode.
+                Defaults to False.
+            fps_on (bool): Boolean switch for activating the fps counter as part of the animation.
+                Defaults to False.
+        """
 
         # Initialise pygame
         pygame.init()
@@ -39,19 +56,41 @@ class ScreenAnimator:
         self._clock = pygame.time.Clock()
 
     def _create_images_group(self) -> None:
+        """
+        Create the group of images.
+
+        Returns:
+            None
+        """
+
         # Create the images group
         self._images = pygame.sprite.Group()
         # Create the images
         self._create_images()
 
     def _create_messages_group(self) -> None:
+        """
+        Create the group of messages.
+
+        Returns:
+            None
+        """
+
         # Create the messages group
         self._messages = pygame.sprite.Group()
-        # Write the first message and add to group
+        # Create a message
         self._create_message()
 
     def run(self) -> None:
-        """Main loop of the animation."""
+        """
+        Run main execution loop of the program. A series of methods check the state of the program nd update as
+        required.
+
+        Returns:
+            None
+        """
+
+        # Run a continuous loop
         while True:
             # Set the FPS
             self._clock.tick(self.settings.settings['fps'])
@@ -69,21 +108,52 @@ class ScreenAnimator:
             self._update_screen()
 
     def _check_events(self) -> None:
-        # Watch for events
+        """
+        Check through all the pygame events and then use methods to check if certain criteria are met.
+
+        Returns:
+            None
+        """
+
+        # Loop through each event
         for event in pygame.event.get():
             # Check if should quit
             self._check_quit(event)
 
     @staticmethod
-    def _check_quit(event) -> None:
+    def _check_quit(event: pygame.event) -> None:
+        """
+        Check whether any of the pygame events meet the criteria for exiting the program, then exit the program if
+        there are.
+
+        Args:
+            event (event): An event in pygame.
+
+        Returns:
+            None
+        """
+
         if event.type == pygame.QUIT or (event.type == pygame.KEYDOWN and event.key == pygame.K_q):
             sys.exit()
 
     def _set_bg(self) -> None:
-        """Set the background of the animation."""
+        """
+        Set the background colour of the animation.
+
+        Returns:
+            None
+        """
+
         self.screen.fill(self.settings.bg_colour)
 
     def _create_images(self) -> None:
+        """
+        Create a number of Picture objects, depending on image source and number of each image requested.
+
+        Returns:
+            None
+        """
+
         # Create all the required images
         for i in range(self.settings.settings['num_images']):
             # Create an image of each type
@@ -92,20 +162,42 @@ class ScreenAnimator:
                 Picture(self._images, self, image)
 
     def _create_message(self) -> None:
-        # Create a message
+        """
+        Creates a Message object.
+
+        Returns:
+            None
+        """
+
         Message(self._messages, self)
 
     def _check_items(self) -> None:
-        # Check whether the message has fully emerged on screen, then create another if so.
+        """
+        Check whether the messages on screen have met certain criteria, then run specific methods depending on
+        criteria met.
+
+        Returns:
+            None
+        """
+
+        # Loop through each message
         for message in self._messages.sprites():
+            # Check whether the message has fully emerged on screen, then create another if so
             if message.has_just_emerged():
                 self._create_message()
 
-            # If the message has left the screen, get rid of it.
+            # If the message has left the screen, get rid of it
             if not message.is_on_screen():
                 message.kill()
 
     def _update_items(self) -> None:
+        """
+        Run update methods for the different animations features.
+
+        Returns:
+            None
+        """
+
         # Update images
         self._update_images()
 
@@ -116,8 +208,17 @@ class ScreenAnimator:
         self._change_colours()
 
     def _update_images(self) -> None:
-        # Update images when required
+        """
+        Check whether enough clock time has passed to update the image positions, run image update method it it has.
+
+        Returns:
+            None
+        """
+
+        # Get the current clock time
         image_change_time_new = pygame.time.get_ticks()
+
+        # Check whether time passed since last image change update has reached the change limit
         if image_change_time_new - self._image_change_time >= self.settings.settings['image_change_time'] * 1000:
             # Update time measurement with new time
             self._image_change_time = image_change_time_new
@@ -130,8 +231,17 @@ class ScreenAnimator:
             self._images = image_group_new
 
     def _change_colours(self) -> None:
-        # Update colours when required
+        """
+        Check whether enough clock time has passed to update the colours used, run colour update method it it has.
+
+        Returns:
+            None
+        """
+
+        # Get the current clock time
         colour_change_time_new = pygame.time.get_ticks()
+
+        # Check whether time passed since last colour change update has reached the change limit
         if colour_change_time_new - self._colour_change_time >= self.settings.settings['colour_change_time'] * 1000:
             # Update time measurement with new time
             self._colour_change_time = colour_change_time_new
@@ -140,6 +250,13 @@ class ScreenAnimator:
             self.settings.set_colours()
 
     def _update_screen(self) -> None:
+        """
+        Update all aspects of the animation, and show the new frame.
+
+        Returns:
+            None
+        """
+
         # Set the background colour
         self._set_bg()
 
@@ -159,11 +276,17 @@ class ScreenAnimator:
         pygame.display.flip()
 
     def _draw_fps(self):
+        """
+        Draw the measured frames per second value that the animation is running at on the screen animation.
+
+        Returns:
+            None
+        """
+
         fps_font = pygame.font.SysFont(self.settings.settings['typeface'], 36)
         text = f'{self._clock.get_fps():.2f}'
         content = fps_font.render(text, True, (0, 0, 0))
         rect = content.get_rect()
-        # screen_rect = self.screen.get_rect()
         rect.x = 10
         rect.y = 10
         self.screen.blit(content, rect)
