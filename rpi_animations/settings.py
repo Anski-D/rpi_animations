fc159fb2eab1ac39c05388686387902bf9752c74
David Anscombe <dave.anski@gmail.com>
2021-12-06 16:14:31 +0000

    Create initial settings.py

---
+++ rpi_animations/settings.py
@@ -0,0 +1,14 @@
+# Module imports
+import json
+import os.path
+
+_SUBDIR = 'inputs'
+_SETTINGS_FILE = 'settings.json'
+
+
+class ImportSettings:
+    """Imports and validates the user settings.
+    """
+    def __init__(self):
+        """Inits ImportSettings with settings file string."""
+        pass
