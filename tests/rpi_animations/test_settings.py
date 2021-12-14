72867bb5da81eb7f4ed263c02b0db8f7ab8f9b98
David Anscombe <dave.anski@gmail.com>
2021-12-14 14:40:18 +0000

    Create tests for settings imports

---
+++ tests/rpi_animations/test_settings.py
@@ -0,0 +1,86 @@
+import pytest
+from rpi_animations.settings import SettingsImporter
+
+
+class TestSettingsImporters:
+    @pytest.fixture
+    def imported_json(self):
+        pytest.imported_json = {
+            "colours": [
+                [255, 0, 0],
+                [0, 255, 0],
+                [0, 0, 255]
+            ],
+            "messages": [
+                "TEST MESSAGE 1!",
+                "TEST MESSAGE 2!",
+                "TEST MESSAGE 3!",
+                "TEST MESSAGE 4!"
+            ],
+            "message_sep": "   ",
+            "typeface": "freeserif",
+            "text_size": 350,
+            "bold_text": 1,
+            "italic_text": 0,
+            "text_aa": 0,
+            "text_speed": 240.0,
+            "outline_width": 3,
+            "outline_colours": [
+                [0, 0, 0],
+                [255, 255, 255]
+            ],
+            "image_sources": [
+                "pic1.bmp",
+                "pic2.bmp",
+                "pic3.bmp"],
+            "num_images": 10,
+            "image_change_time": 2,
+            "colour_change_time": 15,
+            "fps": 30,
+            "reposition_attempts": 50
+        }
+        pytest.settings_dict = {
+            "colours": [
+                (255, 0, 0),
+                (0, 255, 0),
+                (0, 0, 255)
+            ],
+            "messages": [
+                "TEST MESSAGE 1!",
+                "TEST MESSAGE 2!",
+                "TEST MESSAGE 3!",
+                "TEST MESSAGE 4!"
+            ],
+            "message_sep": "   ",
+            "typeface": "freeserif",
+            "text_size": 350,
+            "bold_text": 1,
+            "italic_text": 0,
+            "text_aa": 0,
+            "text_speed": 240.0,
+            "outline_width": 3,
+            "outline_colours": [
+                (0, 0, 0),
+                (255, 255, 255)
+            ],
+            "image_sources": [
+                "pic1.bmp",
+                "pic2.bmp",
+                "pic3.bmp"],
+            "num_images": 10,
+            "image_change_time": 2,
+            "colour_change_time": 15,
+            "fps": 30,
+            "reposition_attempts": 50
+        }
+
+    @pytest.fixture
+    def settings_importer_dummy_input(self, imported_json, monkeypatch):
+        monkeypatch.setattr(SettingsImporter, '_read_settings', lambda x: None)
+        settings_importer = SettingsImporter('dummy')
+        settings_importer._settings = pytest.imported_json
+        return settings_importer
+
+    def test_settings_import_convert_colours(self, imported_json, settings_importer_dummy_input, monkeypatch):
+        settings_importer_dummy_input._convert_colours()
+        assert settings_importer_dummy_input._settings == pytest.settings_dict
