b26515915f1920b48bb13ee36a9dc9eb6b9346c0
David Anscombe <dave.anski@gmail.com>
2022-04-09 18:23:06 +0100

    Massive rewrite of the Item class, add additional movement class

--- rpi_animations/items.py
+++ rpi_animations/items.py
@@ -1,50 +1,68 @@
 from abc import ABC, abstractmethod
-from pygame.sprite import Sprite
+from pygame import Rect
+from pygame.sprite import Group, Sprite
 import random
 
 
 class Item(ABC, Sprite):
-    def __init__(self, group: 'pygame.sprite.Group', content, perimeter: 'pygame.Rect', movement=None) -> None:
-        """
-
-        Args:
-            group:
-            content:
-            perimeter:
-        """
+    def __init__(self, group: Group, content, perimeter: Rect, movement=None) -> None:
         super().__init__(group)
+        self._rect = None
+        self._perimeter = None
         self.content = content
-        self._movement = movement
+        self.perimeter = perimeter
+        self.movement = movement(self)
+
+    @property
+    def rect(self):
+        return self._rect
+
+    @rect.setter
+    def rect(self, rect: Rect):
+        if isinstance(rect, Rect):
+            self._rect = rect
 
     @property
     def content(self):
-        """
-
-        Returns:
-
-        """
         return self._content
 
     @content.setter
     def content(self, content) -> None:
-        """
-
-        Args:
-            content:
-
-        Returns:
-
-        """
         self._content = content
+        self._rect = self._content.get_rect()
 
-        if self._rect is None:
-            self._rect = self._content.get_rect()
+    @property
+    def perimeter(self):
+        return self._perimeter
+
+    @perimeter.setter
+    def perimeter(self, perimeter):
+        if isinstance(perimeter, Rect):
+            self._perimeter = perimeter
+
+    @property
+    def movement(self):
+        return self._movement
+
+    @movement.setter
+    def movement(self, movement: 'Movement'):
+        if isinstance(movement, Movement):
+            self._movement = movement
+
+    @classmethod
+    def create_scrolling_item(cls, group, content, perimeter):
+        return Item(group, content, perimeter, movement=ScrollingMovement)
+
+    @classmethod
+    def create_random_item(cls, group, content, perimeter):
+        return Item(group, content, perimeter, movement=RandomMovement)
 
     def set_position(self, item_ref, target_position):
         self._rect.__setattr__(item_ref, target_position)
 
-    def move(self, *args, **kwargs):
-        self._movement.move(*args, **kwargs)
+    def move(self):
+        if self._movement is not None:
+            self._movement.move()
 
 
 class Message(Item):
@@ -120,3 +138,37 @@ class Picture(Item):
     def _set_position(self):
         self._rect.left = random.randint(0, self._perimeter.right - self._rect.width)
         self._rect.top = random.randint(0, self._perimeter.bottom - self._rect.height)
+
+
+class Movement:
+    def __init__(self, item: Item):
+        self._item = item
+
+    @abstractmethod
+    def move(self):
+        pass
+
+
+class ScrollingMovement(Movement):
+    def __init__(self, item: Item, speed=0):
+        super().__init__(item)
+        self.speed = speed
+
+    @property
+    def speed(self):
+        return self._speed
+
+    @speed.setter
+    def speed(self, speed):
+        if isinstance(speed, (int, float)):
+            self._speed = speed
+
+    def move(self, speed=None):
+        self.speed = speed
+        self._item.set_position('x', self._item.rect.x - self._speed)  # pixel/frame
+
+
+class RandomMovement(Movement):
+    def move(self):
+        self._item.set_position('left', random.randint(0, self._item.perimeter.right - self._item.rect.width))
+        self._item.set_position('top', random.randint(0, self._item.perimeter.bottom - self._item.rect.height))
