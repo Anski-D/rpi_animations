6677f178b40fd51a95c9e2f0e3cb92eba66f9f62
David Anscombe <dave.anski@gmail.com>
2022-04-14 20:41:02 +0100

    Write start of init in Canvas

--- rpi_animations/canvas.py
+++ rpi_animations/canvas.py
@@ -1,2 +1,8 @@
+from .items import Item
+from .settings import SettingsManager
+import pygame as pg
+
+
 class Canvas:
-    pass
+    def __init__(self, settings_manager: SettingsManager):
+        self.settings_manager = settings_manager
